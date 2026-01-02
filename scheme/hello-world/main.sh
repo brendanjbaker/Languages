93d0ebd9f3878b10a1564b2f470928be8955e3e8
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-01 22:18:39 -0500

    Use environment variable to disable Guile auto-compilation.

--- scheme/hello-world/main.sh
+++ scheme/hello-world/main.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+export GUILE_AUTO_COMPILE=0
+
 if [[ ! -f program.go ]]; then
 	guild compile program.scm --output=program.go > /dev/null
 fi
 
-guile --no-auto-compile -c '(load-compiled "program.go")'
+guile -c '(load-compiled "program.go")'
