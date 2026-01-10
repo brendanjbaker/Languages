a6992f46a504f3d44887251c166f3307e6fc58db
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-10 16:54:39 -0500

    Improve whitespace-finding script.

--- .scripts/find-space-indentation.sh
+++ .scripts/find-space-indentation.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-grep -r '    ' \
-| grep -Fv 'README.md' \
-| grep -Fv 'languages.sh' \
-| grep -Fv '.git/' \
-| grep -Fv '.scripts/' \
-| grep -Fv 'elm/' \
-| grep -Fv 'fsharp' \
-| grep -Fv 'assembly/'
+grep -r '^    ' \
+| grep -v '^\.git/' \
+| grep -v '^cobol/' \
+| grep -v '^elm/' \
+| grep -v '^fsharp/' \
+|| true
