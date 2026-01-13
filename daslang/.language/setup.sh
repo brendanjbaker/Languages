29d9f0f7004b8402b9509aa43af752364057f2a8
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-13 01:17:51 -0500

    Add Daslang language.

---
+++ daslang/.language/setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get install -y --no-install-recommends \
+	bison \
+	cmake \
+	flex \
+	git \
+	libgl1-mesa-dev \
+	libglu1-mesa-dev \
+	liburiparser-dev \
+	libxcursor-dev \
+	libxi-dev \
+	libxinerama-dev \
+	libxrandr-dev
+
+mkdir -p '/opt/daslang'
+pushd '/opt/daslang'
+git clone 'https://github.com/GaijinEntertainment/daScript' '0.5.9.2.1'
+pushd '0.5.9.2.1'
+git checkout 'v0.5.9.2.1' 2> /dev/null
+git submodule update --init
+git config -f modules/dasQuirrel/.gitmodules submodule.quirrel.url 'https://github.com/GaijinEntertainment/quirrel.git'
+git submodule update --init --recursive
+mkdir -p build
+pushd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target daslang --config Release --parallel "$(nproc)"
+ln -s /opt/daslang/0.5.9.2.1/bin/daslang /usr/bin/daslang
+popd
+rm -rf !(bin|dasgen|daslib|dastest|include|lib)
+popd; popd
---
+++ daslang/hello/main.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+daslang program.das -- "$@"
