8af46961f695caa7375c67368712fa69441d8da8
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-13 23:10:31 -0500

    Suppress compilation output.

--- clu/sum/main.sh
+++ clu/sum/main.sh
@@ -3,7 +3,7 @@
 if [[ ! -f program ]]; then
 	export CLUHOME="/opt/pclu/current"
 
-	pclu -ext false -spec program -opt -cfiles -compile program
+	pclu -ext false -spec program -opt -cfiles -compile program > /dev/null
 	plink -o program program.o
 fi
 
