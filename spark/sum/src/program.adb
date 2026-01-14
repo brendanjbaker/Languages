2c0fab5b481b2056ffbb50e98b5d176e216f4321
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-13 23:12:01 -0500

    Fix Spark language compiler warning re: unprovable operation(s).

--- spark/sum/src/program.adb
+++ spark/sum/src/program.adb
@@ -1,9 +1,32 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure Program with SPARK_Mode
 is
    Sum : Integer := 0;
+
+   --------
+   --  By default, calling this will raise a compiler warning re: provability:
+   --
+   --     Put (Item => Sum, Width => 0);
+   --
+   --  However, using this prints with a leading space because it leaves room
+   --  for the negative sign:
+   --
+   --     Put (Integer'Image (Sum));
+   --
+   --  Therefore, we'll introduce this helper function to print an integer
+   --  without whitespace.
+   --
+   function PrintInteger (N : Integer) return String is
+      Imaged : constant String := Integer'Image (N);
+   begin
+      if N >= 0 then
+         return Imaged (Imaged'First + 1 .. Imaged'Last);
+      else
+         return Imaged;
+      end if;
+   end PrintInteger;
+
 begin
    for I in 0 .. 100 loop
       pragma Loop_Invariant
@@ -11,6 +34,6 @@ begin
       Sum := Sum + I;
    end loop;
 
-   Put(Item => Sum, Width => 0);
+   Put (PrintInteger (Sum));
    New_Line;
 end Program;
