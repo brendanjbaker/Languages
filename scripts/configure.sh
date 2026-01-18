d39a10887e9de9307b567dbe913e23e21c0e74c9
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-17 22:44:16 -0500

    Require root for --configure (don't try to use sudo inside, it might not exist and/or be configured).

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,13 +2,23 @@
 
 set -Eeuo pipefail
 
+function ensure_root {
+	if [[ "$EUID" -ne 0 ]]; then
+		echo "Must be root."
+		exit 1
+	fi
+}
+
 function install_podman {
 	if which 'podman' > /dev/null 2>&1; then
 		return
 	fi
 
+	ensure_root
+
 	if is_debian; then
-		sudo apt-get install -y --no-install-recommends podman
+		apt-get update
+		apt-get install -y --no-install-recommends podman
 	elif is_msys; then
 		winget install --scope machine "RedHat.Podman"
 	else
