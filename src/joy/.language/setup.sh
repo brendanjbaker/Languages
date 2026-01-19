555cbf4dda09b237b475d58b9390e6a24a1dd96a
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-18 20:23:28 -0500

    Fix typo; pin version/commit.

--- src/joy/.language/setup.sh
+++ src/joy/.language/setup.sh
@@ -11,11 +11,12 @@ mkdir -p '/opt/joy'
 pushd '/opt/joy'
 git clone 'https://github.com/Wodan58/Joy.git' '1.59'
 pushd '1.59'
+git checkout '7fbeebcdc15147e7b24760e42869997a02be0f95'
 mkdir 'build'
 pushd 'build'
 cmake -G "Unix Makefiles" ..
 cmake --build .
 cp ../lib/usrlib.joy ~
-mkir ~/usrlib
+mdkir ~/usrlib
 cp ../lib/* ~/usrlib
 popd; popd; popd
