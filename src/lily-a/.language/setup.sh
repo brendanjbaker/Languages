b7cf39d18c16e71098a20c70e222f249498ef3a8
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-20 18:54:19 -0500

    Add other Lily language (this one is more developed, so I'm calling it Lily "A").

---
+++ src/lily-a/.language/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get install -y --no-install-recommends \
+	cmake \
+	gcc \
+	git
+
+mkdir '/opt/lily'
+pushd '/opt/lily'
+git clone --depth 1 --branch 'v2.3' 'https://gitlab.com/FascinatedBox/lily.git' '2.3'
+pushd '2.3'
+cmake .
+cmake --build .
+ln -s '/opt/lily/2.3/lily' '/usr/bin/lily'
+popd; popd
---
+++ src/lily-a/README.md
@@ -0,0 +1,19 @@
+# Lily
+
+This is the Lily language as found here:
+
+https://gitlab.com/FascinatedBox
+
+The language's syntax looks like this:
+
+```
+define sum(begin: *Integer = 5, end: *Integer = 10): Integer {
+	var result: Integer = 0
+
+	for i in begin...end: {
+		result += i
+	}
+
+	return result
+}
+```
