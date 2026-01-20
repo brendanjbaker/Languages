330eb23018141210c8f6c30c0ac108008ebb880a
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-19 22:33:33 -0500

    Add Par language.

---
+++ src/par/.language/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get install -y --no-install-recommends \
+	git \
+	libssl-dev \
+	pkg-config \
+	rustup
+
+rustup install 1.92.0
+rustup default 1.92.0
+
+mkdir '/opt/par'
+pushd '/opt/par'
+git clone 'https://github.com/faiface/par-lang' '0.1.0'
+pushd '0.1.0'
+git checkout 'd42c75efa6af4effaf808c4bf09eb7df28b8dd40' 2> /dev/null
+cargo install --path .
+popd; popd
+ln -s '/opt/par/0.1.0/target/release/par-lang' '/usr/bin/par-lang'
---
+++ src/par/hello/main.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+par-lang run program.par "$@"
