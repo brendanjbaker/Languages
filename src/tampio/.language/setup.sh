5419338aa800ed51126bd948a51e823b3125d53e
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-28 00:30:58 -0500

    Tampio is language version 1.46.

--- src/tampio/.language/setup.sh
+++ src/tampio/.language/setup.sh
@@ -23,9 +23,23 @@ unzip 'dict-morpho.zip' -d ~/.voikko
 popd
 rm -fr 'tmp/tampio'
 
+# The first time running, the Tampio compiler emits these warnings:
+#
+#     /opt/tampio/1.46/lex.py:233: SyntaxWarning: invalid escape sequence '\s'
+#       return self.isComment() or not not re.fullmatch("\s*", self.token)
+#     /opt/tampio/1.46/lex.py:235: SyntaxWarning: invalid escape sequence '\('
+#       return not not re.fullmatch("#[^\n]*\n|\([^()]*\)", self.token)
+#
+function swallow_initial_warning {
+	pushd '/opt/tampio/1.46'
+	python3 /opt/tampio/1.46/tampio.py /opt/tampio/1.46/std.itp > /dev/null 2>&1
+	popd
+}
+
 mkdir '/opt/tampio'
 pushd '/opt/tampio'
-git clone 'https://github.com/fergusq/tampio.git' 'current'
-pushd 'current'
+git clone 'https://github.com/fergusq/tampio.git' '1.46'
+pushd '1.46'
 git checkout 'b0017d2557a21c47fddc20cfceeb92f4e0376401' 2> /dev/null
+swallow_initial_warning
 popd; popd
--- src/tampio/hello/main.sh
+++ src/tampio/hello/main.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ ! -f program.js ]]; then
-	export TAMPIO_HOME='/opt/tampio/current'
+	python3 "/opt/tampio/1.46/tampio.py" --print-included program.itp > program.js
 
-	python3 "${TAMPIO_HOME}/tampio.py" "${TAMPIO_HOME}/std.itp" >> program.js
-	python3 "${TAMPIO_HOME}/tampio.py" program.itp >> program.js
+	declare main_method
 
-	declare main_method=$(cat program.js | grep '^function ' | tail -n -1 | awk '{ print $2; }')
+	main_method=$(cat program.js | grep '^function ' | tail -n -1 | awk '{ print $2; }')
 
 	echo "${main_method};" >> program.js
 fi
