dc67257f17dc65cba86fa38338751808e5cd76f5
Brendan J. Baker <brendan@brendanbaker.com>
2026-01-30 00:10:31 -0500

    Use HTTPS instead of HTTP for package download.

--- src/caml/.language/setup.sh
+++ src/caml/.language/setup.sh
@@ -8,7 +8,7 @@ mkdir '/tmp/caml'
 pushd '/tmp/caml'
 
 download \
-	--url 'http://boisson.homeip.net/depot/pool/xenial/amd64/camllight_0.82-6_amd64.deb' \
+	--url 'https://boisson.homeip.net/depot/pool/xenial/amd64/camllight_0.82-6_amd64.deb' \
 	--hash '2e95a05ef096ce9a6ee6339fe5f8c96b08fad97b'
 
 dpkg -i --force-depends 'camllight_0.82-6_amd64.deb'
