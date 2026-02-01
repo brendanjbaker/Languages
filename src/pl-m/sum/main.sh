b82aed2b71014ee8fc12f65f5d9ce1bed45bdd21
Brendan J. Baker <brendan@brendanbaker.com>
2026-02-01 00:12:31 -0500

    Output cleanup.

--- src/pl-m/sum/main.sh
+++ src/pl-m/sum/main.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ ! -f PROGRAM.COM ]]; then
-	uplm80 'PROGRAM.PLM' -o 'PROGRAM.MAC'
-	um80 'PROGRAM.MAC' -o 'PROGRAM.REL'
-	ul80 -o 'PROGRAM.COM' 'PROGRAM.REL'
+	uplm80 'PROGRAM.PLM' -o 'PROGRAM.MAC' > /dev/null
+	um80 'PROGRAM.MAC' -o 'PROGRAM.REL' > /dev/null
+	ul80 -o 'PROGRAM.COM' 'PROGRAM.REL' > /dev/null
 	cp 'PROGRAM.COM' 'A/0/'
 fi
+
+export TERM='xterm'
 
 runcpm > output <<- EOF
 	PROGRAM
