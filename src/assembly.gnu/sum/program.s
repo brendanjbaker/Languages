43e4882da6b613f31bb8a200883616689fadc14c
Brendan J. Baker <brendan@brendanbaker.com>
2026-02-04 02:47:04 -0500

    Whitespace.

--- src/assembly.gnu/sum/program.s
+++ src/assembly.gnu/sum/program.s
@@ -1,46 +1,46 @@
 .section .data
 buffer:
-    .space 32
+	.space 32
 
 .section .text
 .global _start
 _start:
-    # sum = 0 + 1 + ... + 100
-    xor     %eax, %eax
-    xor     %ecx, %ecx
+	# sum = 0 + 1 + ... + 100
+	xor     %eax, %eax
+	xor     %ecx, %ecx
 
 sum_loop:
-    add     %ecx, %eax
-    inc     %ecx
-    cmp     $101, %ecx
-    jl      sum_loop
+	add     %ecx, %eax
+	inc     %ecx
+	cmp     $101, %ecx
+	jl      sum_loop
 
-    # convert EAX to decimal ASCII
-    lea     buffer+31(%rip), %rsi
-    movb    $'\n', (%rsi)
-    dec     %rsi
+	# convert EAX to decimal ASCII
+	lea     buffer+31(%rip), %rsi
+	movb    $'\n', (%rsi)
+	dec     %rsi
 
-    mov     $10, %ebx
+	mov     $10, %ebx
 
 convert_loop:
-    xor     %edx, %edx
-    div     %ebx
-    add     $'0', %dl
-    mov     %dl, (%rsi)
-    dec     %rsi
-    test    %eax, %eax
-    jnz     convert_loop
+	xor     %edx, %edx
+	div     %ebx
+	add     $'0', %dl
+	mov     %dl, (%rsi)
+	dec     %rsi
+	test    %eax, %eax
+	jnz     convert_loop
 
-    inc     %rsi            # start of string
+	inc     %rsi            # start of string
 
-    # write(1, rsi, buffer+32 - rsi)
-    mov     $1, %rax        # sys_write
-    mov     $1, %rdi        # stdout
-    lea     buffer+32(%rip), %rdx
-    sub     %rsi, %rdx      # length
-    syscall
+	# write(1, rsi, buffer+32 - rsi)
+	mov     $1, %rax        # sys_write
+	mov     $1, %rdi        # stdout
+	lea     buffer+32(%rip), %rdx
+	sub     %rsi, %rdx      # length
+	syscall
 
-    # exit(0)
-    mov     $60, %rax       # sys_exit
-    xor     %rdi, %rdi
-    syscall
+	# exit(0)
+	mov     $60, %rax       # sys_exit
+	xor     %rdi, %rdi
+	syscall
--- src/assembly.microsoft/sum/program.asm
+++ src/assembly.microsoft/sum/program.asm
@@ -12,55 +12,55 @@ bytes   dq ?
 
 .code
 main proc
-    sub rsp, 40               ; shadow space + alignment
+	sub rsp, 40               ; shadow space + alignment
 
-    ; -----------------------------
-    ; sum = 0 + 1 + ... + 100
-    ; -----------------------------
-    xor eax, eax              ; sum = 0
-    xor ecx, ecx              ; i = 0
+	; -----------------------------
+	; sum = 0 + 1 + ... + 100
+	; -----------------------------
+	xor eax, eax              ; sum = 0
+	xor ecx, ecx              ; i = 0
 
 sum_loop:
-    add eax, ecx
-    inc ecx
-    cmp ecx, 101
-    jl  sum_loop              ; loop while i < 101
+	add eax, ecx
+	inc ecx
+	cmp ecx, 101
+	jl  sum_loop              ; loop while i < 101
 
-    ; -----------------------------
-    ; convert EAX to decimal ASCII
-    ; -----------------------------
-    lea rdi, buffer + 31      ; write digits backwards
-    mov byte ptr [rdi], 10    ; newline
-    dec rdi
+	; -----------------------------
+	; convert EAX to decimal ASCII
+	; -----------------------------
+	lea rdi, buffer + 31      ; write digits backwards
+	mov byte ptr [rdi], 10    ; newline
+	dec rdi
 
-    mov ebx, 10
+	mov ebx, 10
 
 convert_loop:
-    xor edx, edx
-    div ebx                   ; EAX /= 10, remainder in EDX
-    add dl, '0'
-    mov [rdi], dl
-    dec rdi
-    test eax, eax
-    jnz convert_loop
+	xor edx, edx
+	div ebx                   ; EAX /= 10, remainder in EDX
+	add dl, '0'
+	mov [rdi], dl
+	dec rdi
+	test eax, eax
+	jnz convert_loop
 
-    inc rdi                   ; point to first digit
+	inc rdi                   ; point to first digit
 
-    ; -----------------------------
-    ; write to stdout
-    ; -----------------------------
-    mov ecx, STD_OUTPUT_HANDLE
-    call GetStdHandle         ; RAX = stdout handle
+	; -----------------------------
+	; write to stdout
+	; -----------------------------
+	mov ecx, STD_OUTPUT_HANDLE
+	call GetStdHandle         ; RAX = stdout handle
 
-    mov rcx, rax              ; hFile
-    mov rdx, rdi              ; lpBuffer
-    lea r8,  buffer + 32
-    sub r8,  rdi               ; length
-    lea r9,  bytes
-    call WriteFile
+	mov rcx, rax              ; hFile
+	mov rdx, rdi              ; lpBuffer
+	lea r8,  buffer + 32
+	sub r8,  rdi               ; length
+	lea r9,  bytes
+	call WriteFile
 
-    xor ecx, ecx
-    call ExitProcess
+	xor ecx, ecx
+	call ExitProcess
 main endp
 
 end
