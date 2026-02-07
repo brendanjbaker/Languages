f158fe621a8d68bc3d8d011cd38a3992d31b97eb
Brendan J. Baker <brendan@brendanbaker.com>
2026-02-07 16:42:04 -0500

    Add Elk language (WIP).

---
+++ src/elk/.language/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get install -y --no-install-recommends ...
+
+mkdir '/tmp/elk'
+pushd '/tmp/elk'
+download --url 'https://github.com/PaddiM8/elk/releases/download/v0.0.6/linux-x64.tar.xz'
+tar -xvf 'linux-x64.tar.xz' -C /
+popd
+rm -fr '/tmp/elk'
---
+++ src/elk/hello/main.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+elk program.elk "$@"
