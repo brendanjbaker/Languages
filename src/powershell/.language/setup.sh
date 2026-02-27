c3cc81cfe0347034d812ab2fc9567cd68cc2f697
Brendan J. Baker <brendan@brendanbaker.com>
2026-02-26 19:59:55 -0500

    Update PowerShell version.

--- src/powershell/.language/setup.sh
+++ src/powershell/.language/setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
-
 export DEBIAN_FRONTEND="noninteractive"
 
 apt-get install -y --no-install-recommends \
-	apt-transport-https \
 	ca-certificates \
-	libicu76 \
 	wget
 
-wget -q 'https://github.com/PowerShell/PowerShell/releases/download/v7.5.4/powershell_7.5.4-1.deb_amd64.deb'
-printf '%s  %s\n' '6ad18a6d8746c0435b8a2f18ecc12e3d7b50eb3e' 'powershell_7.5.4-1.deb_amd64.deb' | sha1sum --check -
-dpkg -i 'powershell_7.5.4-1.deb_amd64.deb' || true
-rm 'powershell_7.5.4-1.deb_amd64.deb'
+mkdir '/tmp/powershell'
+pushd '/tmp/powershell'
+wget -q 'https://github.com/PowerShell/PowerShell/releases/download/v7.6.0-rc.1/powershell-preview_7.6.0-rc.1-1.deb_amd64.deb'
+printf '%s  %s\n' 'de3e264276db102e6d4b4c4d9a06684f0f4bd833' 'powershell-preview_7.6.0-rc.1-1.deb_amd64.deb' | sha1sum --check -
+apt-get install -y --no-install-recommends './powershell-preview_7.6.0-rc.1-1.deb_amd64.deb'
+popd
+rm -fr '/tmp/powershell'
+ln -s '/usr/bin/pwsh-preview' '/usr/bin/pwsh'
