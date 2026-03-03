544e42d0b43c6a8b591167ce72e99d8d0790f1ff
Brendan J. Baker <brendan@brendanbaker.com>
2026-03-02 23:04:07 -0500

    Improve C# "sum" program.

--- src/csharp/sum/Program.cs
+++ src/csharp/sum/Program.cs
@@ -4,13 +4,18 @@ public static class Program
 {
 	public static void Main()
 	{
+		Console.WriteLine(SumRange(0, 100));
+	}
+
+	private static int SumRange(int begin, int end)
+	{
 		int sum = 0;
 
-		for (int i = 0; i <= 100; i++)
+		for (int n = begin; n <= end; n++)
 		{
-			sum += i;
+			sum += n;
 		}
 
-		Console.WriteLine(sum);
+		return sum;
 	}
 }
