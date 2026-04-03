6f2b280c8a7489cc70527b2ab9f102513786c605
Brendan J. Baker <brendan@brendanbaker.com>
2026-04-03 19:19:55 -0400

    Build Pony from source (binary download fails constantly).

--- src/pony/.language/setup.sh
+++ src/pony/.language/setup.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
-###
-# Build from source:
+# The recommended way to install Pony is via PonyUp:
 #
-# git clone --depth 1 --branch 0.60.4 https://github.com/ponylang/ponyc.git
-# cd ponyc
-# make libs
-# make configure
-# make build
-# make install
+# https://github.com/ponylang/ponyup
 #
+# Version 0.15.1 of the "ponyup-init.sh" file is here:
+#
+# https://raw.githubusercontent.com/ponylang/ponyup/d16d27eb6cab9c458a4a4272dcbcd8cac8424d98/ponyup-init.sh
+#
+# However, it usually fails to successfully download the necessary file(s) from
+# CloudSmith. Therefore, we will build from source.
 
 export DEBIAN_FRONTEND="noninteractive"
 
 apt-get install -y --no-install-recommends \
+	build-essential \
 	ca-certificates \
-	clang \
-	curl
+	cmake \
+	git \
+	lld \
+	python3
 
-export SHELL=/usr/bin/bash
-
-sh -cx "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)" || true
-
-export PATH="/root/.local/share/ponyup/bin:$PATH"
-
-ponyup default x86_64-linux-ubuntu24.04
-ponyup update ponyc release-0.60.4
+mkdir '/opt/ponyc'
+pushd '/opt/ponyc'
+git clone 'https://github.com/ponylang/ponyc.git' '0.62.1'
+pushd '0.62.1'
+git checkout '49f73be6ed5c61735e65874a5991866828312114' 2> /dev/null
+make libs
+make configure
+make build
+make install
+popd; popd
