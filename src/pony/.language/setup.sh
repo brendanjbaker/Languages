a3eeee869b21d0b8e4916ba1c10a2364a1f86e72
Brendan J. Baker <brendan@brendanbaker.com>
2026-04-04 11:58:35 -0400

    Build Pony from source. Includes a troublesome LLVM build, needed some extra magic/flags to get the LLVM build to complete successfully -- not sure if it's an LLVM issue, or if it's related to limited resources inside a container.

--- src/pony/.language/setup.sh
+++ src/pony/.language/setup.sh
@@ -19,14 +19,34 @@ apt-get install -y --no-install-recommends \
 	cmake \
 	git \
 	lld \
-	python3
+	python3 \
+	zlib1g-dev
+
+# When building LLVM using gcc, this error happens:
+#
+# >   [ 47%] Building CXX object llvm/src/llvm/lib/Analysis/CMakeFiles/LLVMAnalysis.dir/BlockFrequencyInfoImpl.cpp.o
+# >   In file included from /opt/ponyc/0.62.1/lib/llvm/src/llvm/lib/Analysis/BlockFrequencyInfoImpl.cpp:14:
+# >   /opt/ponyc/0.62.1/lib/llvm/src/llvm/include/llvm/ADT/APInt.h: In member function 'llvm::APInt& llvm::APInt::clearUnusedBits()':
+# >   /opt/ponyc/0.62.1/lib/llvm/src/llvm/include/llvm/ADT/APInt.h:1989:37: internal compiler error: Illegal instruction
+function llvm_build_fix {
+	export CFLAGS="-march=x86-64"
+	export CXXFLAGS="-march=x86-64"
+}
+
+LLVM_CONFIGURE_ARGS="\
+-DLLVM_TARGETS_TO_BUILD=X86 \
+-DLLVM_BUILD_TOOLS=OFF \
+-DLLVM_BUILD_TESTS=OFF \
+-DLLVM_BUILD_EXAMPLES=OFF"
 
 mkdir '/opt/ponyc'
 pushd '/opt/ponyc'
 git clone 'https://github.com/ponylang/ponyc.git' '0.62.1'
 pushd '0.62.1'
 git checkout '49f73be6ed5c61735e65874a5991866828312114' 2> /dev/null
-make libs
+git submodule update --init --recursive --depth 1
+llvm_build_fix
+make -j1 libs LLVM_CONFIGURE_ARGS="$LLVM_CONFIGURE_ARGS"
 make configure
 make build
 make install
