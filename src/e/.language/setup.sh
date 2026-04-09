9212cff16f725e63584ce16e497707ac50ad788f
Brendan J. Baker <brendan@brendanbaker.com>
2026-04-09 19:29:16 -0400

    Fix E language: erights.org went offline, but I found the .tar.gz file (same SHA1) on GitHub. Lucky!

--- src/e/.language/setup.sh
+++ src/e/.language/setup.sh
@@ -9,13 +9,13 @@ apt-get install -y --no-install-recommends \
 
 mkdir '/tmp/e'
 pushd '/tmp/e'
-wget -q --no-check-certificate 'https://erights.org/download/0-9-3/E-purej-0.9.3d.tar.gz'
+wget -q 'https://raw.githubusercontent.com/erights/erights-org-website/refs/heads/master/download/0-9-3/E-purej-0.9.3d.tar.gz'
 printf '%s  %s\n' 'f7ffe78cf89339c019a0bf6c349e5be25d42a2ce' 'E-purej-0.9.3d.tar.gz' | sha1sum --check -
-tar -xvf 'E-purej-0.9.3d.tar.gz'
+tar -xf 'E-purej-0.9.3d.tar.gz'
 mkdir dist
 shopt -s extglob
 cp -r !(dist) dist/
-bash inst-e.bash
+bash 'inst-e.bash'
 popd
 rm -fr '/tmp/e'
 ln -s '/usr/local/e/bin/rune' '/usr/bin/rune'
