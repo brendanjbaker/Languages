7948bd9dcecbefb8aa1c6217279f14f5b10580ef
Brendan J. Baker <brendan@brendanbaker.com>
2026-05-20 00:23:26 -0400

    Add Flower language (hello and sum programs).

--- src/flower/.language/setup.sh
+++ src/flower/.language/setup.sh
@@ -8,12 +8,15 @@ apt-get install -y --no-install-recommends \
 	clang \
 	git
 
-mkdir '/opt/flower'
-pushd '/opt/flower'
+mkdir '/tmp/flower'
+pushd '/tmp/flower'
 git clone 'https://github.com/IvyMycelia/flower.git' '9a2e425'
 pushd '9a2e425'
 git checkout '9a2e4251e142f31c2a9389f102a78311cd245bfa' 2> /dev/null
-gcc bin/Flower.c -o bin/Flower
-# make -f MAKEFILE build
+gcc 'bin/Flower.c' -o 'bin/Flower_c'
+./bin/Flower_c 'src/main.flo' 'bin/Flower_f'
+mkdir -p '/opt/flower/9a2e425'
+mv 'bin/Flower_f' '/opt/flower/9a2e425/Flower_f'
 popd; popd
-ln -s '/opt/flower/9a2e425/bin/Flower' '/usr/bin/flower'
+rm -fr '/tmp/flower'
+ln -s '/opt/flower/9a2e425/Flower_f' '/usr/bin/flower'
